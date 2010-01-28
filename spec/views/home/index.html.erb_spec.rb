53c86716a2383aec68f79eb210ed8e692b9f6aed
Gilles MATHURIN <gillesmath@macmini.local>
2010-01-28 17:07:17 -0400

    debut d'implementation du menu slider

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe "/home/index" do
+  
+  it "displays the menu bar with the home link selected" do
+    render
+    response.should have_selector("li.selected", :content => "Accueil")
+  end
 
   context "with existing articles and events" do
     before(:each) do
@@ -25,6 +30,26 @@ describe "/home/index" do
       render
       response.should have_selector(".next_events") do |list|
         list.should have_selector("li.event", :content => "the title")
+      end
+    end
+  end
+  
+  context "with no contents to display" do
+    before(:each) do
+      assigns[:articles], assigns[:events] = [], []
+    end
+    
+    it "display a no articles messages" do
+      render
+      response.should have_selector(".recent_articles") do |list|
+          list.should have_selector("li.article", :content => "Pas d'articles pour le moment")
+      end
+    end
+    
+    it "displays a No next events to come" do
+      render
+      response.should have_selector(".next_events") do |list|
+        list.should have_selector("li.event", :content => "Pas d'événements à venir pour le moment")
       end
     end
   end
