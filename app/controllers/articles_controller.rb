ead27715bb45448abb354c9d14e6d84152f3e4cf
Gilles Mathurin <gillesmathurin@new-host-3.home>
2010-01-31 09:32:37 -0400

    fin de l'implementation du controleur articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,5 +33,28 @@ class ArticlesController < ApplicationController
     end
   end
   
+  def update
+    @article = Article.find(params[:id])
+    
+    respond_to do |format|
+      if @article.update_attributes(params[:article])
+        flash[:notice] = "Article modifié avec succès"
+        format.html { redirect_to(@article) }
+        format.xml { render :xml => @article, :status => :updated, :location => @article }
+      else
+        format.html { render :action => "update" }
+        format.xml { render :xml => @article.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to(articles_url) }
+    end
+  end
 
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,14 +1,9 @@
 require 'spec_helper'
 
 describe ArticlesController do
-
-  #Delete these examples and add some real ones
-  it "should use ArticlesController" do
-    controller.should be_an_instance_of(ArticlesController)
-  end
   
-  before(:each) do
-    @article = mock_model(Article)
+  def mock_article(stubs={})
+    @mock_article ||= mock_model(Article, stubs)
   end
 
   describe "GET 'index'" do
@@ -47,16 +42,70 @@ describe ArticlesController do
   end
   
   describe "POST 'create'" do
-    context "with a successful save" do
-      it "assigns description" do
-        
+    context "with valid params" do
+      it "assigns a newly created article as @article" do
+        Article.stub(:new).with({'these' => "params"}).and_return(mock_article)
+        mock_article.should_receive(:save).and_return(true)
+        post 'create', :article => {:these => "params"}
+        assigns[:article].should equal(mock_article)
+      end
+      
+      it "redirect to the created article" do
+        Article.stub(:new).and_return(mock_article(:save => true))
+        post 'create', :article => {}
+        response.should redirect_to(article_url(mock_article))
       end
     end
     
-    context "with a unsuccessful save" do
-      it "should description" do
-        
+    context "with invalid params" do
+      it "assigns a newly created but unsaved article as @article" do
+        Article.stub(:new).with({'these' => "params"}).and_return(mock_article(:save => false))
+        post :create, :article => {:these => 'params'}
+        assigns[:article].should equal(mock_article)
       end
+      
+      it "re-renders the 'new' template" do
+        Article.stub(:new).and_return(mock_article(:save => false))
+        post :create, :article => {}
+        response.should render_template('new')
+      end
+    end
+  end
+  
+  describe "PUT update" do
+    context "with valid params" do
+      it "updates the requested article" do
+        Article.should_receive(:find).with("1").and_return(mock_article)
+        mock_article.should_receive(:update_attributes).with('these' => 'params')
+        put :update, :id => "1", :article => {:these => 'params'}
+      end
+      
+      it "assigns the requested article as @article" do
+        Article.stub(:find).and_return(mock_article(:update_attributes => true))
+        put :update, :id => "1"
+        assigns[:article].should equal(mock_article)
+      end
+      
+      it "redirects to the article" do
+        Article.stub(:find).and_return(mock_article(:update_attributes => true))
+        put :update, :id => "1"
+        response.should redirect_to(article_url(mock_article))
+      end
+    end
+  end
+  
+  describe "DELETE destroy" do
+    it "destroys the requested article" do
+      Article.should_receive(:find).with("1").and_return(mock_article)
+      mock_article.should_receive(:destroy)
+      delete :destroy, :id => "1"
+    end
+    
+    it "redirects to the articles list" do
+      Article.should_receive(:find).with("1").and_return(mock_article)
+      mock_article.should_receive(:destroy)
+      delete :destroy, :id => "1"
+      response.should redirect_to(articles_url)
     end
   end
 end
