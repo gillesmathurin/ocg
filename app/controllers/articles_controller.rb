f8fb9ec3573f90aac7d8f41696a28ba4cf537028
Gilles Mathurin <gillesmath@mac.com>
2010-01-31 08:55:23 -0400

    debut d'implementation du controleur articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,37 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.paginate(:page => params[:page], :per_page => 10, :order => "created_at desc")
   end
 
   def show
+    @article = Article.find(params[:id])
+    respond_to do |format|
+      format.html {  }
+    end
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
+  
+  def create
+    @article = Article.new(params[:article])
+  
+    respond_to do |format|
+      if @article.save
+        flash[:notice] = 'Article crée avec succès.'
+        format.html { redirect_to(@article) }
+        format.xml { render :xml => @article, :status => :created, :location => @article }
+      else
+        format.html { render :action => "new" }
+        format.xml { render :xml => @article.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
 
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -6,33 +6,57 @@ describe ArticlesController do
   it "should use ArticlesController" do
     controller.should be_an_instance_of(ArticlesController)
   end
-
+  
+  before(:each) do
+    @article = mock_model(Article)
+  end
 
   describe "GET 'index'" do
-    it "should be successful" do
+    it "find all the articles as @articles by group of 10" do
+      Article.should_receive(:paginate).and_return([@article])
       get 'index'
-      response.should be_success
+      assigns[:articles].should == [@article]
     end
   end
 
   describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
-      response.should be_success
+    it "find the requested article as @article" do
+      Article.should_receive(:find).with("1").and_return(@article)
+      get 'show', :id => "1"
+      assigns[:article].should == @article
+      response.should render_template('show')
     end
   end
 
   describe "GET 'new'" do
-    it "should be successful" do
+    it "assigns a new article as @article" do
+      Article.should_receive(:new).and_return(@article)
       get 'new'
-      response.should be_success
+      assigns[:article].should == @article
+      response.should render_template("new")
     end
   end
 
   describe "GET 'edit'" do
-    it "should be successful" do
-      get 'edit'
-      response.should be_success
+    it "assigns the requested article as @article" do
+      Article.should_receive(:find).with("1").and_return(@article)
+      get 'edit', :id => "1"
+      assigns[:article].should == @article
+      response.should render_template("edit")
+    end
+  end
+  
+  describe "POST 'create'" do
+    context "with a successful save" do
+      it "assigns description" do
+        
+      end
+    end
+    
+    context "with a unsuccessful save" do
+      it "should description" do
+        
+      end
     end
   end
 end
