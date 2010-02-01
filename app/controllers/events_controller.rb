959b7e3053a4d132e85ec7415967ad8f1fffb1dd
Gilles MATHURIN <gillesmath@macmini.local>
2010-01-31 21:18:27 -0400

    Implementation de events_controller.rb

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,51 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.paginate(:page => params[:page], :per_page => 10, :order => "start_date desc")
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+    @event = Event.new
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
-
+  
+  def create
+    @event = Event.new(params[:event])
+    
+    respond_to do |format|
+      if @event.save
+        flash[:notice] = 'Evenement enregistré.'
+        format.html { redirect_to(@event) }
+        format.xml { render :xml => @event, :status => :created, :location => @event }
+      else
+        format.html { render :action => "new" }
+        format.xml { render :xml => @event.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    
+    respond_to do |format|
+       if @event.update_attributes(params[:event])
+         flash[:notice] = "Evenement modifié"
+         format.html { redirect_to(event_url(@event)) }
+       else
+         format.html { render :action => "edit" }
+       end
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to eventS_url
+  end
 end
--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,37 +2,79 @@ require 'spec_helper'
 
 describe EventsController do
 
-  #Delete these examples and add some real ones
-  it "should use EventsController" do
-    controller.should be_an_instance_of(EventsController)
+  def mock_event(stubs = {})
+    @mock_event ||= mock_model(Event, stubs)
   end
 
-
   describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
+    it "assigns the 10 most recent events as @events" do
+      Event.stub(:paginate).and_return([mock_event])
+      get :index
+      assigns[:events].should == ([mock_event])
     end
   end
 
   describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
-      response.should be_success
+    it "assigns the requested event as @event" do
+      Event.should_receive(:find).with("37").and_return(mock_event)
+      get :show, :id => "37"
+      assigns[:event].should == mock_event
     end
   end
 
   describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
+    it "assigns a newly created but unsaved event as @event" do
+      Event.should_receive(:new).and_return(mock_event)
+      get :new
+      assigns[:event].should == mock_event
     end
   end
 
   describe "GET 'edit'" do
-    it "should be successful" do
-      get 'edit'
-      response.should be_success
+    it "assigns the requested event as @event" do
+      Event.should_receive(:find).with("37").and_return(mock_event)
+      get :edit, :id => "37"
+      assigns[:event].should == mock_event
+    end
+  end
+  
+  describe "POST 'create'" do
+    context "with valid params" do
+      it "assigns a newly created event as @event" do
+        Event.should_receive(:new).with({'these' => 'params'}).and_return(mock_event(:save => true))
+        post :create, :event => {:these => "params"}
+        assigns[:event].should == mock_event
+      end
+      
+      it "redirect to the newly created event page" do
+        Event.should_receive(:new).with({'these' => 'params'}).and_return(mock_event(:save => true))
+        post :create, :event => {:these => "params"}
+        response.should redirect_to(event_url(mock_event))
+      end
+    end
+    
+    context "with invalid params" do
+      it "re-renders the new template" do
+        Event.should_receive(:new).with({}).and_return(mock_event(:save => false))
+        post :create, :event => {}
+        response.should render_template("new")
+      end
+    end
+  end
+  
+  describe "PUT 'update'" do
+    context "with valid params" do
+      it "does something" do
+        
+      end
+    end
+    
+    context "with invalid params" do
+      it "re-renders the edit template" do
+        Event.should_receive(:find).with("37").and_return(mock_event(:update_attributes => false))
+        put :update, :id => "37"
+        response.should render_template('edit')
+      end
     end
   end
 end
