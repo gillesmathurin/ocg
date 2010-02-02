9ec58212b37cfd5711fad3f6c1ad0c3fe4941e11
Gilles Mathurin <gillesmathurin@new-host-3.home>
2010-02-01 23:03:02 -0400

    Ajout du programme de la federation  du carnaval et des fetes de la guadeloupe

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,4 +17,11 @@ Event.create(:title => "Election Reine et Roi du carnaval", :place => "Bouillant
 Event.create(:title => "Défilé à Trois Rivières - Défilé des Ecoles de Basse-Terre", :place => "Trois Rivière et Basse-Terre", :start_date => "01-31-2010", :end_date => "01-31-2010")
 Event.create(:title => "Village TAMBOU VAPEUR", :place => "Bouillante", :description => "Défilé", :start_date => "02-06-2010", :end_date => "02-06-2010")
 Event.create(:title => "Défilé des écoles + Les ainés de Bouillante", :place => "Bouillante", :description => "", :start_date => "02-07-2010", :end_date => "02-07-2010")
-Event.create(:title => "")
+Event.create(:title => "Défilé des écoles", :place => "Trois Rivières", :start_date => "02-13-2010", :end_date => "02-13-2010", :description => "")
+Event.create(:title => "Défilé à Pointe-à-Pitre", :place => "Pointe-à-Pitre", :start_date => "02-14-2010", :end_date => "02-14-2010")
+Event.create(:title => "Parade électrique", :place "", :start_date => "02-15-2010", :end_date => "02-15-2010", :description => "")
+Event.create(:title => "Winner 2010 Grande Parade", :place => "", :description => "Char de la Gauloise avec NRJ, Bal de la FEDE au swing-club avec TIPICAL ABIDOS")
+Event.create(:title => "Mercredi des cendres à Basse-Terre", :place => "Basse-Terre", :start_date => "02-15-2010", :end_date => "02-15-2010")
+Event.create(:title => "Mi Careme", :start_date => "03-11-2010",:end_date => "03-11-2010")
+
+# TODO : Updater les evenements ci-dessus avec le champ Federation = "Federation du carnaval et des fetes de la Guadeloupe"
