bc36946f02df94d0533337a247dc20d869bbbad3
Gilles MATHURIN <gillesmath@macmini.local>
2010-02-04 16:09:12 -0400

    optimisation de l'affichage du programme

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   
   has_attached_file :photo, :styles => { :thumb => "200x200>" }
   
-  named_scope :next_4, :conditions => ['start_date > ?', Date.today], :limit => 4 
+  named_scope :next_4, :conditions => ['start_date > ?', Date.today], :limit => 5 
   FEDERATIONS = ['Fédération du Carnaval et des Fêtes de la Guadeloupe', 'Fédération Guadeloupéenne de Carnaval', 'Groupement pour le Carnaval et la Culture Région Pointoise']
   
   named_scope :fcfg, :conditions => ['federation = ?', "Fédération du Carnaval et des Fêtes de la Guadeloupe"]
