295f8fc5a1df4bd7cb788021bac51bd4fb5b9783
Gilles MATHURIN <gillesmath@macmini.local>
2010-02-11 16:59:25 -0400

    implentation de la vue articles/sow.html.erb

--- spec/views/articles/show.html.erb_spec.rb
+++ spec/views/articles/show.html.erb_spec.rb
@@ -4,9 +4,4 @@ describe "/articles/show" do
   before(:each) do
     render 'articles/show'
   end
-
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/articles/show])
-  end
 end
