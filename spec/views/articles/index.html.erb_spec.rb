f3709afcee8ec19c1e3388aa1dd75cc50ca2c39d
Gilles MATHURIN <gillesmath@macmini.local>
2010-02-18 14:14:06 -0400

    articles/index and articles/index.html.erb_spec.rb pass

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -7,6 +7,7 @@ describe "/articles/index" do
     before(:each) do
       @article = mock_model(Article, :title => "a title", :start_date =>Time.parse("2010-02-10 15:00:00"), :content => "")
       assigns[:articles] = [ @article ]
+      @user = create_default_user
     end
     
     it "renders some articles summary" do
@@ -16,13 +17,32 @@ describe "/articles/index" do
       end
     end
     
-    it "renders links with each article" do
-      # pending()
+    it "renders the Lire link" do
       render
-      response.should have_selector(".articles") do |list|
-        list.should have_selector("a", :href => article_path(@article))
-        list.should have_selector("a", :href => edit_article_path(@article))
-        list.should have_selector("a", :href => article_path(@article))
+      response do |list|
+        list.should have_selector("li.article", :content => "a title")
+        list.should contain("Lire")
+        list.should_not contain("Modifier")
+        list.should_not contain("Supprimer")
+      end
+    end
+    
+    context "with a user signed_in" do
+      before(:each) do
+        sign_in(@user)
+      end
+      
+      it "renders links with each article" do
+        render
+        response.should have_selector(".articles") do |list|
+          list.should have_selector("a", :href => edit_article_path(@article))
+          list.should contain("Modifier")
+          list.should contain("Supprimer")
+        end
+      end
+      
+      after(:each) do
+        sign_out(@user)
       end
     end
   end
