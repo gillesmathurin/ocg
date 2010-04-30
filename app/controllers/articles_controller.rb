f00a9ca5130f264d732e154ea765954ee4ad373e
Gilles MATHURIN <gillesmath@macmini.local>
2010-04-30 09:52:33 -0400

    modification mineure dans articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :edit, :create, :destroy, :update]
-  uses_tiny_mce
+  uses_tiny_mce# :options => { :theme => 'advanced' }
   
   def index
     @articles = Article.paginate(:page => params[:page], :per_page => 10, :order => "created_at desc")
