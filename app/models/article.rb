884367d6e7719cb684fee9f9861c5ac902e0bd63
Gilles MATHURIN <gillesmath@macmini.local>
2010-05-08 01:44:09 -0400

    Gestion d'un document attaché à un article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :paragraphs,:dependent => :destroy
   has_attached_file :photo, :styles => { :thumb => "200x200>" }
+  has_attached_file :document
   accepts_nested_attributes_for :paragraphs, :allow_destroy => true
   
   named_scope :recent_articles, :order => "created_at desc", :limit => 4  
