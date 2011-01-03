98357766fe739d8f1aebb68d84d135c7a5f414f7
Gilles MATHURIN <gillesmath@macmini.local>
2011-01-03 10:14:49 -0400

    fixed Event.to_come method

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ActiveRecord::Base
   named_scope :fcfg, :conditions => ['federation = ? AND start_date >= ?', "Fédération du Carnaval et des Fêtes de la Guadeloupe", Date.today]
   named_scope :gccrp, :conditions => ['federation = ? AND start_date >= ?', "Groupement pour le Carnaval et la Culture Région Pointoise", Date.today]
   named_scope :fcg, :conditions => ['federation = ? AND start_date >= ?', "Fédération Guadeloupéenne de Carnaval", Date.today]
-  named_scope :to_come, :conditions => ['start_date > ?', Date.today]
+  named_scope :to_come, :conditions => ['start_date >= ? OR end_date >= ?', Date.today, Date.today]
   
   
   def to_param
