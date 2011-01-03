b722c2afa313c6e7199391cabca0b53ee45b77c3
Gilles MATHURIN <gillesmath@macmini.local>
2011-01-03 10:42:58 -0400

    slight optimizations

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,13 @@ class Event < ActiveRecord::Base
   
   has_attached_file :photo, :styles => { :thumb => "200x200>" }
   
-  named_scope :next_10, :conditions => ['start_date >= ? OR end_date >= ?', Date.today, Date.today], :limit => 10 
+  named_scope :next_10, :conditions => ['end_date >= ?', Date.today], :limit => 10 
   FEDERATIONS = ['Fédération du Carnaval et des Fêtes de la Guadeloupe', 'Fédération Guadeloupéenne de Carnaval', 'Groupement pour le Carnaval et la Culture Région Pointoise']
   
   named_scope :fcfg, :conditions => ['federation = ? AND start_date >= ?', "Fédération du Carnaval et des Fêtes de la Guadeloupe", Date.today]
   named_scope :gccrp, :conditions => ['federation = ? AND start_date >= ?', "Groupement pour le Carnaval et la Culture Région Pointoise", Date.today]
   named_scope :fcg, :conditions => ['federation = ? AND start_date >= ?', "Fédération Guadeloupéenne de Carnaval", Date.today]
-  named_scope :to_come, :conditions => ['start_date >= ? OR end_date >= ?', Date.today, Date.today]
+  named_scope :to_come, :conditions => ['end_date >= ?', Date.today]
   
   
   def to_param
