07672c5ab5d8a8d67b660eac23a069b62250603d
Gilles Mathurin <gillesmath@me.com>
2012-01-10 14:36:18 -0400

    added new Comite&Federation to the Event model FEDERATIONs constant array

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   has_attached_file :photo, :styles => { :thumb => "200x200>" }
   
   named_scope :next_10, :conditions => ['end_date >= ?', Date.today], :limit => 10 
-  FEDERATIONS = ['Fédération du Carnaval et des Fêtes de la Guadeloupe', 'Fédération Guadeloupéenne de Carnaval', 'Groupement pour le Carnaval et la Culture Région Pointoise']
+  FEDERATIONS = ['Fédération du Carnaval et des Fêtes de la Guadeloupe', 'Fédération Guadeloupéenne de Carnaval', 'Groupement pour le Carnaval et la Culture Région Pointoise', 'Kolektif Mas Kiltirèl', 'Comite du Carnaval de Marie-Galante']
   
   named_scope :fcfg, :conditions => ['federation = ? AND start_date >= ?', "Fédération du Carnaval et des Fêtes de la Guadeloupe", Date.today]
   named_scope :gccrp, :conditions => ['federation = ? AND start_date >= ?', "Groupement pour le Carnaval et la Culture Région Pointoise", Date.today]
