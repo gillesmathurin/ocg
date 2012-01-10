123cb59fc8bffc85f0a83d2270ec855e7f4665c7
Gilles Mathurin <gillesmath@me.com>
2012-01-10 17:33:32 -0400

    updated app directory to rails 3.1.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ocg::Application.config.secret_token = '51bfe478f16a67585da8226cf8ee29599d5d3f7106f7e1da8fdc10cb4987674460dc0fb9909a1008b01a2cfab480eec9ad508f246a6d56d75cdf2c62d00d43ef'
--- vendor/gems/whenever-0.5.0/lib/whenever/version.rb
+++
@@ -1,3 +0,0 @@
-module Whenever
-  VERSION = '0.5.0' 
-end unless defined?(Whenever::VERSION)
