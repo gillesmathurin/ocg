c104d15d0d31992f0fccdf76c11b39b0add61f9b
Gilles Mathurin <gillesmath@me.com>
2012-01-11 19:02:46 -0400

    started to refactored design and layouts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,32 @@ $(function() {
     theme: 'advanced'
   });
 });
+
+// Menu effect plugin
+$(function() { $(".lavaLamp").lavaLamp({ fx: "linear", speed: 300 })});
+
+// helper function for the anythingSlider plugin
+function formatText(index, panel) { return index + ""; }
+
+// call of the anythingSlider plugin
+$(function() {
+
+    $('.anythingSlider').anythingSlider({
+        easing: "easeInOutExpo",        // Anything other than "linear" or "swing" requires the easing plugin
+        autoPlay: true,                 // This turns off the entire FUNCTIONALY, not just if it starts running or not.
+        delay: 3000,                    // How long between slide transitions in AutoPlay mode
+        startStopped: false,            // If autoPlay is on, this can force it to start stopped
+        animationTime: 600,             // How long the slide transition takes
+        hashTags: true,                 // Should links change the hashtag in the URL?
+        buildNavigation: false,          // If true, builds and list of anchor links to link to each slide
+		pauseOnHover: true,             // If true, and autoPlay is enabled, the show will pause on hover
+		startText: "Go",             // Start text
+    stopText: "Stop",               // Stop text
+    navigationFormatter: formatText       // Details at the top of the file on this use (advanced use)
+    });
+    
+    $("#slide-jump").click(function(){
+        $('.anythingSlider').anythingSlider(6);
+    });
+    
+});
