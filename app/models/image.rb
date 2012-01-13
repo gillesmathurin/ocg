29a3263ba4ffee296a07b6f217b8f85d6552460c
Gilles Mathurin <gillesmath@me.com>
2012-01-13 19:40:14 -0400

    redesigned the galery index page

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,3 @@
 class Image < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :thumb => "155x155>", :large => "500x500>" }
+  has_attached_file :photo, :styles => { :sthumb => "90x90>", :thumb => "155x155>", :large => "500x500>" }
 end
