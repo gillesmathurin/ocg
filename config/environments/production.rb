514f41a0e0b0f10a267fde22a55dd20d821ec552
Gilles Mathurin <gillesmath@me.com>
2012-01-17 15:19:25 -0400

    fixed environment/production.rb mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Ocg::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'www.ocguadeloupe.org' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default = { :charset => "iso-8859-1" }
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com", 
     :port => 587, 
@@ -60,7 +61,6 @@ Ocg::Application.configure do
   }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_charset = "iso-8859-1"
   
   # Enable threaded mode
   # config.threadsafe!
